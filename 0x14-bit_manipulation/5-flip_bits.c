6674d795c33629da21246a87c6f60430251ad969
edwin nyongesa <arjannky@gmail.com>
2023-05-04 11:37:36 -0700

    qtask 5 on flipping bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,32 @@
+/*
+ * File: 5-flip_bits.c
+ * Auth: edwin nyongesa
+ * Date: May 4th, 2023
+ */
+
+#include "holberton.h"
+
+/**
+ * flip_bits - Calculates the number of bits that are different between
+ *             two given numbers in their binary representation.
+ *
+ * @n: The first number.
+ * @m: The second number.
+ *
+ * Return: The number of bits that are different between n and m.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m;
+	unsigned int bits = 0;
+
+	/* Count the number of different bits between n and m */
+	while (xor > 0)
+	{
+		bits += (xor & 1);
+		xor >>= 1;
+	}
+
+	return (bits);
+}
+
