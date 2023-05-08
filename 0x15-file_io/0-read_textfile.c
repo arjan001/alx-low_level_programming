4ce2f47e028a130de81a6dd8d597c022fe113a89
edwin nyongesa <arjannky@gmail.com>
2023-05-08 04:41:46 -0700

    fixing file quiz zero

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,23 +1,24 @@
 /*
-* File: 0-read_textfile.c
-* Auth: edwin nyongesa
-*/
+ * File: 0-read_textfile.c
+ * Auth: edwin nyongesa
+ */
 
 #include "main.h"
 #include <stdlib.h>
 
 /**
-* read_textfile - Read and print a given number of characters from a text file
-*                 to standard output.
-* @filename: Pointer to the name of the file to be read.
-* @chars: Number of characters to read and print.
-*
-* Return: 0 if the function fails or filename is NULL,
-*         otherwise the actual number of bytes the function can read and print.
-*/
+ * read_textfile - Read and print a given number of characters from a text file
+ *                  to standard outut.
+ * @filename: Pointer to the name of the file to be read.
+ * @chars: Number of characters to read and print.
+ *
+ * Return: 0 if the function fails or filename is NULL,
+ *          otherwise the actual number of bytes the function can
+ *          read and print.
+ */
 ssize_t read_textfile(const char *filename, size_t chars)
 {
-	ssize_t fd, read_chars, written_chars;
+	ssize_t fd, r_chars, w_chars;
 	char *buf;
 
 	if (filename == NULL)
@@ -28,10 +29,10 @@ ssize_t read_textfile(const char *filename, size_t chars)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	read_chars = read(fd, buf, chars);
-	written_chars = write(STDOUT_FILENO, buf, read_chars);
+	r_chars = read(fd, buf, chars);
+	w_chars = write(STDOUT_FILENO, buf, r_chars);
 
-	if (fd == -1 || read_chars == -1 || written_chars == -1 || written_chars != read_chars)
+	if (fd == -1 || r_chars == -1 || w_chars == -1 || w_chars != r_chars)
 	{
 		free(buf);
 		return (0);
@@ -40,6 +41,6 @@ ssize_t read_textfile(const char *filename, size_t chars)
 	free(buf);
 	close(fd);
 
-	return (written_chars);
+	return (w_chars);
 }
 
