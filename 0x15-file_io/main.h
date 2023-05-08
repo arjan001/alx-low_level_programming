35c0a01bfe4987dd18e249b3ac73467baaf9cc0d
edwin nyongesa <arjannky@gmail.com>
2023-05-07 22:40:49 -0700

    fixed main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,8 +14,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN_H *
+#endif /* MAIN_H */
